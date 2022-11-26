91a80b7a900c16983259a52f1e40d1626b66406e
Chris Palmer <chris@noncombatant.org>
2022-11-25 20:30:37 -0800

    Enable more iterations than fit in a stack frame

--- kr_malloc_test.c
+++ kr_malloc_test.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "get_utc_nanoseconds.h"
@@ -9,15 +10,23 @@
 #define iterations 1000
 
 int main() {
-  void* ps[iterations];
+  const size_t iterations_size = iterations * sizeof(char*);
+  char** ps = malloc(iterations_size);
+  // Touch every page to ensure the benchmark doesn’t get noisier than it
+  // already is due to lazy commitment/page faults.
+  for (size_t i = 0; i < iterations_size; i += 4096) {
+    ps[i] = NULL;
+  }
+
   const int64_t start = GetUTCNanoseconds();
 
   for (size_t i = 1; i < iterations; i++) {
 #ifdef ORIGINAL_FLAVOR
-    void* p = ps[i] = kr_malloc(i * 1);
+    char* p = kr_malloc(i * 1);
 #else
-    void* p = ps[i] = kr_malloc(i, 1);
+    char* p = kr_malloc(i, 1);
 #endif
+    ps[i] = p;
     if (p == NULL) {
       printf("%s\n", strerror(errno));
       return errno;
