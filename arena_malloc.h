d963e24f1d82809bbed681ae9862a14ed37ed6f5
Chris Palmer <chris@noncombatant.org>
2022-11-26 01:56:44 -0800

    Move the public declarations up for readability

--- arena_malloc.h
+++ arena_malloc.h
@@ -1,51 +1,18 @@
 #include <assert.h>
 #include <stddef.h>
 
-// No user-serviceable parts inside.
-typedef struct Header {
-  struct Header* next;
-  size_t unit_count;
-  // Add padding if the `static_assert` below fires.
-  // char _[count];
-} Header;
-
-// `long double` is (probably) the largest machine type. (If it’s not on your
-// machine, change this `typedef`.) To ensure that allocations are properly
-// aligned for all machine types, we assert that `Header` (the minimum unit of
-// allocation) has the right size. This may require padding.
-typedef long double Alignment;
-static_assert(sizeof(Header) == sizeof(Alignment), "Add padding to `Header`");
-
-// No user-serviceable parts inside.
-typedef struct Chunk {
-  struct Chunk* next;
-  size_t byte_count;
-} Chunk;
-
-// No user-serviceable parts inside. See `arena_create`.
-typedef struct Arena {
-  Chunk* chunk_list;
-
-  Header free_list;
-
-  // `NULL` is a sentinel value indicating that `free_list` has not yet been
-  // initialized.
-  Header* free_list_start;
-
-  // We always request at least this amount from the operating system. The value
-  // should be chosen (a) to reduce pressure on the page table; and (b) to
-  // reduce
-  // the number of times we need to invoke the kernel.
-  size_t minimum_chunk_size;
-} Arena;
-
-// This is a good value to use for `Arena.minimum_chunk_size` for most
-// applications. It is tuned to be appropriate for the platform.
-extern size_t default_minimum_chunk_size;
+// `Arena` is a metadata structure that describes a (set of) allocation
+// region(s). You can use 1 for the entire process, or 1 per thread, or 1 per
+// object lifetime, or whatever you like.
+typedef struct Arena Arena;
 
 // Initializes the new `Arena`, setting its `minimum_chunk_size`, which is
 // measured in `sizeof(Header)` _units_.
 void arena_create(Arena* a, size_t minimum_chunk_size);
+
+// This is a good value to use for `Arena.minimum_chunk_size` for most
+// applications. It is tuned to be appropriate for the platform.
+extern size_t default_minimum_chunk_size;
 
 // Returns a pointer to a memory region containing at least `count * size`
 // bytes. Checks the multiplication for overflow.
@@ -61,3 +28,40 @@ void arena_free(Arena* a, void* p);
 // Returns all memory in the `Arena` back to the platform. All allocations made
 // inside the arena will be invalid after this function returns.
 void arena_destroy(Arena* a);
+
+// Implementation details below this point.
+
+typedef struct Header {
+  struct Header* next;
+  size_t unit_count;
+  // Add padding if the `static_assert` below fires.
+  // char _[count];
+} Header;
+
+// `long double` is (probably) the largest machine type. (If it’s not on your
+// machine, change this `typedef`.) To ensure that allocations are properly
+// aligned for all machine types, we assert that `Header` (the minimum unit of
+// allocation) has the right size. This may require padding.
+typedef long double Alignment;
+static_assert(sizeof(Header) == sizeof(Alignment), "Add padding to `Header`");
+
+typedef struct Chunk {
+  struct Chunk* next;
+  size_t byte_count;
+} Chunk;
+
+struct Arena {
+  Chunk* chunk_list;
+
+  Header free_list;
+
+  // `NULL` is a sentinel value indicating that `free_list` has not yet been
+  // initialized.
+  Header* free_list_start;
+
+  // We always request at least this amount from the operating system. The value
+  // should be chosen (a) to reduce pressure on the page table; and (b) to
+  // reduce
+  // the number of times we need to invoke the kernel.
+  size_t minimum_chunk_size;
+};
